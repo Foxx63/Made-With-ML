7ca65d263ec1fcf0b0b11872982816edde55555f
Foxx63 <Jamespelumi01@gmail.com>
2023-10-02 17:14:51 +0100

    added document splitting

--- main.py
+++ main.py
@@ -15,8 +15,25 @@ from langchain.document_loaders import PyPDFLoader
 loader = PyPDFLoader("text.pdf")
 pages = loader.load()
 page = pages[0]
-
 # print(page.page_content)
+
+from langchain.text_splitter import CharacterTextSplitter, RecursiveCharacterTextSplitter
+
+text_splitter_1 = CharacterTextSplitter( separator= "", chunk_size = 150, chunk_overlap = 10 , length_function = len )
+
+docs = text_splitter_1.split_documents(pages)
+
+
+print(len(docs))
+print(len(pages))
+
+
+
+
+
+
+
+
 
 
 
