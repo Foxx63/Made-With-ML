41d1a3eb198c7472a81d4f54e2c8a5a714f5ba70
Foxx63 <Jamespelumi01@gmail.com>
2023-10-09 14:14:11 +0100

    ported the code to a jupyter kernel

--- main.py
+++ main.py
@@ -11,11 +11,12 @@ _ = load_dotenv(find_dotenv()) # read local .env file
 openai.api_key  = os.environ['OPENAI_API_KEY']
 
 
+
 from langchain.document_loaders import PyPDFLoader
 loader = PyPDFLoader("text.pdf")
 pages = loader.load()
 page = pages[0]
-# print(page.page_content)
+print(page.page_content)
 
 from langchain.text_splitter import CharacterTextSplitter, RecursiveCharacterTextSplitter
 
@@ -28,7 +29,12 @@ print(len(docs))
 print(len(pages))
 
 
+from langchain.embeddings.openai import OpenAIEmbeddings
+embeddings = OpenAIEmbeddings
 
+sentence1 = "i like dogs"
+sentence2 = "i like canines"
+sentence3 = "the weather is ugly outside"
 
 
 
